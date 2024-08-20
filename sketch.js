73bda7dc644ce82507253541d681b6b734711e72
Giada Sun <giada1198@gmail.com>
2024-08-20 12:54:46 -0700

    clean code

--- sketch.js
+++ sketch.js
@@ -1,25 +1,8 @@
-
-let angle = 0;
 let state = 'init';
-let canvas_3d;
-
-let letterOpacity = 1;
-
 let language = 'tc';
+let is_mobile;
 
-let daggerZ = 0;
-
-let letterRotateX = 0;
-let letterRotateY = 0;
-
-let letterY = 150;
-
-let letterWiggleX = 0;
-let letterWiggleXPositive = true;
-let letterWiggleY = 0;
-let letterWiggleYPositive = true;
-
-let frontCountdown = 20;
+let canvas_3d;
 
 let letter_front_desktop_img, letter_back_desktop_img, letter_back_desktop_en_img;
 let letter_front_mobile_img, letter_back_mobile_img;
@@ -42,21 +25,33 @@ const next_button_mobile_img_w = 240;
 const next_button_mobile_img_h = 43;
 const next_button_mobile_y_offset = 134;
 
-let is_mobile;
-
 const survey_link = 'https://forms.gle/AvYd9YZ7U9vqC8Vx5';
 
+let letter_opacity = 0;
+let front_countdown = 20;
+
+let letter_y_offset = 150;
+let letter_rotate_x = 0;
+let letter_rotate_y = 0;
+
+let letter_tilt_x = 0;
+let letter_tilt_x_forward = true;
+let letter_tilt_y = 0;
+let letter_tilt_y_forward = true;
+
+let dagger_z = 0;
+
 function preload() {
-	// Letter
+	// letter
 	letter_front_desktop_img = loadImage('assets/letter-front-desktop.png');
 	letter_back_desktop_img = loadImage('assets/letter-back-desktop.png');
 	letter_back_desktop_en_img = loadImage('assets/letter-back-desktop-en.png');
 	letter_front_mobile_img = loadImage('assets/letter-front-mobile.png');
 	letter_back_mobile_img = loadImage('assets/letter-back-mobile.png');
 	letter_back_mobile_en_img = loadImage('assets/letter-back-mobile-en.png');
-	// Dagger
+	// dagger
 	daggerImg = loadImage('assets/dagger.png');
-	// Button
+	// button
 	language_button_tc_img = loadImage('assets/language-button-tc.png');
 	language_button_en_img = loadImage('assets/language-button-en.png');
 	next_button_desktop_img = loadImage('assets/next-button-desktop.png');
@@ -75,8 +70,6 @@ function setup() {
 	}
 	let w = Math.max(320, windowWidth);
 	let h = Math.max(min_h, windowHeight);
-	console.log('w:', w, "h:", h);
-
 	createCanvas(w, h);
 	canvas_3d = createGraphics(w, h, WEBGL);
 	canvas_3d.noStroke();
@@ -109,67 +102,37 @@ function draw() {
 	background(0);
 	canvas_3d.clear();
 	canvas_3d.push();
-	canvas_3d.translate(0, letterY, 0);
+	canvas_3d.translate(0, letter_y_offset, 0);
+	canvas_3d.rotateX(letter_rotate_x);
+	canvas_3d.rotateY(letter_rotate_y);
 
-	if (state === 'back') {
-		letterWiggleX = letterWiggleXPositive? letterWiggleX+(PI/48)/60 : letterWiggleX-(PI/48)/60;
-		letterWiggleY = letterWiggleYPositive? letterWiggleY+(PI/48)/80 : letterWiggleY-(PI/48)/80;
-		
-		if (letterWiggleX >= PI/48) {
-			letterWiggleXPositive = false;
-		} else if (letterWiggleX <= -PI/48) {
-			letterWiggleXPositive = true;
-		}
-
-		if (letterWiggleY >= PI/48) {
-			letterWiggleYPositive = false;
-		} else if (letterWiggleY <= -PI/48) {
-			letterWiggleYPositive = true;
-		}
-
-		canvas_3d.rotateX(letterWiggleX);
-		canvas_3d.rotateY(letterWiggleY);
-
-		// Mouse tracking tilt effect on desktop
-		if (!is_mobile) {
-			canvas_3d.rotateX(map(mouseY, 0, height, -PI/48, PI/48));
-			canvas_3d.rotateY(map(mouseX, 0, width,  -PI/48, PI/48));
-		}
-	}
-
-	canvas_3d.rotateX(letterRotateX);
-	canvas_3d.rotateY(letterRotateY);
-
-	// Draw the front side of the letter
+	// draw the front side of the letter
 	if (is_mobile) {
 		let w = Math.max(Math.min(500, windowWidth), 320);
 		let h = w*(letter_front_mobile_img.height/letter_front_mobile_img.width);
-		//
 		canvas_3d.texture(letter_front_mobile_img);
 		canvas_3d.beginShape();
-		canvas_3d.vertex(-w/2, -h/2, 0, 0, 0);                          							// Top left corner
+		canvas_3d.vertex(-w/2, -h/2, 0, 0, 0);                          								// Top left corner
 		canvas_3d.vertex(w/2, -h/2, 0, letter_front_mobile_img.width, 0);               				// Top right corner
 		canvas_3d.vertex(w/2, h/2, 0, letter_front_mobile_img.width, letter_front_mobile_img.height);	// Bottom right corner
-		canvas_3d.vertex(-w/2, h/2, 0, 0, letter_front_mobile_img.height);               			// Bottom left corner
+		canvas_3d.vertex(-w/2, h/2, 0, 0, letter_front_mobile_img.height);               				// Bottom left corner
 		canvas_3d.endShape(CLOSE);
 	} else {
 		let w = 700;
 		let h = w*(letter_front_desktop_img.height/letter_front_desktop_img.width);
-		//
 		canvas_3d.texture(letter_front_desktop_img);
 		canvas_3d.beginShape();
-		canvas_3d.vertex(-w/2, -h/2, 0, 0, 0);                         								// Top left corner
+		canvas_3d.vertex(-w/2, -h/2, 0, 0, 0);                         									// Top left corner
 		canvas_3d.vertex(w/2, -h/2, 0, letter_front_desktop_img.width, 0);               				// Top right corner
 		canvas_3d.vertex(w/2, h/2, 0, letter_front_desktop_img.width, letter_front_desktop_img.height);	// Bottom right corner
 		canvas_3d.vertex(-w/2, h/2, 0, 0, letter_front_desktop_img.height);          				    // Bottom left corner
 		canvas_3d.endShape(CLOSE);
 	}
 
-	// Draw the back side of the letter
+	// draw the back side of the letter
 	if (is_mobile) {
 		let w = Math.max(Math.min(500, windowWidth), 320);
 		let h = w*(letter_back_mobile_img.height/letter_back_mobile_img.width);
-		//
 		if (language === 'en') {
 			canvas_3d.texture(letter_back_mobile_en_img);
 		} else {
@@ -199,15 +162,15 @@ function draw() {
 	}
 
 	let daggerOffset = 0;
-	if (daggerZ > 0) {
-		daggerOffset = Math.pow(1.3, daggerZ);
+	if (dagger_z > 0) {
+		daggerOffset = Math.pow(1.3, dagger_z);
 	}
 
 	canvas_3d.translate(260+daggerOffset, -270-daggerOffset, 60+2*daggerOffset);
 	canvas_3d.rotateY(-PI/12);
 	canvas_3d.rotateZ(PI/4);
 
-	canvas_3d.tint(255, Math.max(0, 255-Math.pow(1.2, daggerZ)));
+	canvas_3d.tint(255, Math.max(0, 255-Math.pow(1.2, dagger_z)));
 	canvas_3d.texture(daggerImg);
 	canvas_3d.beginShape();
 	canvas_3d.vertex(-daggerImg.width/2, -daggerImg.height/2, 10, 0, 0);
@@ -229,7 +192,7 @@ function draw() {
 	} else if (state === 'language') {
 		language_button_tc.display();
 		language_button_en.display();
-		if (language_button_tc.over() || language_button_en.over()) {
+		if (language_button_tc.is_hovered() || language_button_en.is_hovered()) {
 			cursor('pointer');
 		} else {
 			cursor('default');
@@ -243,41 +206,45 @@ function draw() {
 			state = 'letter-fade-in'
 		}
 	} else if (state === 'letter-fade-in') {
-		letterOpacity += 2;
-		if (letterY <= 0) {
-			letterY = 0;
+		letter_opacity += 2;
+		if (letter_y_offset <= 0) {
+			letter_y_offset = 0;
 		} else {
-			letterY -= 1.5;
+			letter_y_offset -= 1.5;
 		}
 		push();
-		tint(255, 255, 255, letterOpacity);
+		tint(255, 255, 255, letter_opacity);
 		image(canvas_3d, 0, 0);
 		pop();
-		if (letterOpacity >= 255 && letterY === 0) {
+		if (letter_opacity >= 255 && letter_y_offset === 0) {
 			state = 'front';
 		}
 	} else if (state === 'front') {
-		// cursor('pointer');
-		frontCountdown -= 1;
-		if (frontCountdown <= 0) {
+		front_countdown -= 1;
+		if (front_countdown <= 0) {
 			state = 'dagger-out';
 		}
 		image(canvas_3d, 0, 0);
 	} else if (state === 'dagger-out') {
 		image(canvas_3d, 0, 0);
-		daggerZ += 1;
-		if (daggerZ >= 40) {
+		dagger_z += 1;
+		if (dagger_z >= 40) {
 			state = 'turn-over';
 		}
 		cursor(ARROW);
 	} else if (state === 'turn-over') {
-		letterRotateX = Math.max(-PI, letterRotateX-=0.12);
-		letterRotateY = Math.min(2*PI, letterRotateY+=0.20);
+		letter_rotate_x = Math.max(-PI, letter_rotate_x-=0.12);
+		letter_rotate_y = Math.min(2*PI, letter_rotate_y+=0.20);
 		image(canvas_3d, 0, 0);
-		if (letterRotateX === -PI && letterRotateY === 2*PI) {
+		if (letter_rotate_x === -PI && letter_rotate_y === 2*PI) {
 			state = 'back'
 		}
 	} else if (state === 'back') {
+
+
+
+
+
 		image(canvas_3d, 0, 0);
 		if (next_button_desktop.opacity <= 255) {
 			next_button_desktop.opacity = Math.min(255, next_button_desktop.opacity += 10);
@@ -287,28 +254,52 @@ function draw() {
 			next_button_mobile.display();
 		} else {
 			next_button_desktop.display();
-			if (next_button_desktop.over()) {
+			if (next_button_desktop.is_hovered()) {
 				cursor('pointer');
 			} else {
 				cursor('default');
 			}
 		}
 	}
+
+
+	if (state === 'back') {
+		letter_tilt_x = letter_tilt_x_forward ? letter_tilt_x+(PI/48)/60 : letter_tilt_x-(PI/48)/60;
+		letter_tilt_y = letter_tilt_y_forward ? letter_tilt_y+(PI/48)/80 : letter_tilt_y-(PI/48)/80;
+		
+		if (letter_tilt_x >= PI/48) {
+			letter_tilt_x_forward = false;
+		} else if (letter_tilt_x <= -PI/48) {
+			letter_tilt_x_forward = true;
+		}
+
+		if (letter_tilt_y >= PI/48) {
+			letter_tilt_y_forward = false;
+		} else if (letter_tilt_y <= -PI/48) {
+			letter_tilt_y_forward = true;
+		}
+
+		canvas_3d.rotateX(letter_tilt_x);
+		canvas_3d.rotateY(letter_tilt_y);
+
+		// Mouse tracking tilt effect on desktop
+		if (!is_mobile) {
+			canvas_3d.rotateX(map(mouseY, 0, height, -PI/48, PI/48));
+			canvas_3d.rotateY(map(mouseX, 0, width,  -PI/48, PI/48));
+		}
+	}
 }
 
-// function mousePressed() {
-// }
-
 function touchEnded() {
-	if (language_button_tc.isClicked()) {
+	if (language_button_tc.is_hovered()) {
 		language = 'tc';
 		state = 'language-buttons-fade-out';
 		cursor('default');
-	} else if (language_button_en.isClicked()) {
+	} else if (language_button_en.is_hovered()) {
 		language = 'en';
 		state = 'language-buttons-fade-out';
 		cursor('default');
-	} else if (next_button_desktop.isClicked() || next_button_mobile.isClicked()) {
+	} else if (next_button_desktop.is_hovered() || next_button_mobile.is_hovered()) {
 		window.open(survey_link);
 	}
 }
@@ -344,7 +335,6 @@ function windowResized() {
 		let y = h/2+next_button_desktop_y_offset;
 		next_button_desktop.position(x, y);
 	}
-	// console.log('w:', w, "h:", h);
 }
 
 class Button {
@@ -369,7 +359,7 @@ class Button {
 	display() {
 		stroke(0);
 		push();
-		if (this.over()) {
+		if (this.is_hovered()) {
 			tint(170, 170, 170, this.opacity);
 		} else {
 			tint(255, this.opacity);
@@ -382,19 +372,11 @@ class Button {
 		pop();
 	}
 
-	over() {
-	  if ((mouseX > this.x) && (mouseX < this.x+this.width) && (mouseY > this.y) && (mouseY < this.y+this.height)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	isClicked() {
+	is_hovered() {
 		if ((mouseX > this.x) && (mouseX < this.x+this.width) && (mouseY > this.y) && (mouseY < this.y+this.height)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-  }
+}
