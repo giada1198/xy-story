f67ce56ce6b1b4aa2628a793a5dec98b36259d7d
Giada Sun <giada1198@gmail.com>
2024-10-07 11:26:29 -0700

    card in progress

--- card.js
+++ card.js
@@ -13,6 +13,11 @@ let dagger_img;
 let card_inner_left_img, card_inner_center_img, card_inner_right_img, card_inner_bottom_img;
 
 
+let cam;
+let isDragging = false;
+let lastX, lastY;
+
+
 const dpi_multiple = 3;
 
 const survey_link = 'https://forms.gle/AvYd9YZ7U9vqC8Vx5';
@@ -45,14 +50,22 @@ function setup() {
 	canvas_3d = createGraphics(w, h, WEBGL);
 	canvas_3d.noStroke();
 
+	cam = canvas_3d.createCamera();
+
+
+
 }
 
 function draw() {
+
+	canvas_3d.orbitControl();
+	
 	background(0);
+	
 	canvas_3d.clear();
-	canvas_3d.push();
+	
 
-
+	
 
 	let w = card_inner_left_img.width/dpi_multiple;
 	let h = card_inner_left_img.height/dpi_multiple;
@@ -60,14 +73,20 @@ function draw() {
 	let w_center = card_inner_center_img.width/dpi_multiple;
 	let h_center = card_inner_center_img.height/dpi_multiple;
 
+	canvas_3d.push();
 	canvas_3d.texture(card_inner_left_img);
 	canvas_3d.beginShape();
-	canvas_3d.vertex(-w-w_center/2, -h/2, 0, 0, 0);                         									// Top left corner
-	canvas_3d.vertex(-w_center/2, -h/2, 0, card_inner_left_img.width, 0);               				// Top right corner
-	canvas_3d.vertex(-w_center/2, h/2, 0, card_inner_left_img.width, card_inner_left_img.height);	// Bottom right corner
-	canvas_3d.vertex(-w-w_center/2, h/2, 0, 0, card_inner_left_img.height);          				    // Bottom left corner
+	canvas_3d.vertex(-w, -h/2, 0, 0, 0);                         						// Top left corner
+	canvas_3d.vertex(0, -h/2, 0, card_inner_left_img.width, 0);               			// Top right corner
+	canvas_3d.vertex(0, h/2, 0, card_inner_left_img.width, card_inner_left_img.height);	// Bottom right corner
+	canvas_3d.vertex(-w, h/2, 0, 0, card_inner_left_img.height);          				// Bottom left corner
+	canvas_3d.translate(-w_center/2, 0, 0);
+	// canvas_3d.rotateY(1.5);
+	canvas_3d.rotateY(1);
 	canvas_3d.endShape(CLOSE);
+	canvas_3d.pop();
 
+	canvas_3d.push();
 	canvas_3d.texture(card_inner_right_img);
 	canvas_3d.beginShape();
 	canvas_3d.vertex(w_center/2, -h/2, 0, 0, 0);                         									// Top left corner
@@ -97,11 +116,33 @@ function draw() {
 
 
 
-
+	
 
 	canvas_3d.pop();
+	
+	
+
+	if (isDragging) {
+		let dx = mouseX - lastX;
+		let dy = mouseY - lastY;
+		// cam.pan(-dx * 0.005);  // Horizontal movement
+		// cam.tilt(dy * 0.005);  // Vertical movement
+		cam.move(-dx, -dy, 0);
+	  }
 
 	image(canvas_3d, 0, 0);
 
+	lastX = mouseX;
+	lastY = mouseY;
+
+	
 
 }
+
+function mousePressed() {
+	isDragging = true;
+  }
+  
+  function mouseReleased() {
+	isDragging = false;
+  }
