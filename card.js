9ee330ae204d360f66eaf5a373bc1334cd6fe086
Giada Sun <giada1198@gmail.com>
2024-10-08 09:45:59 -0700

    Update card.js

--- card.js
+++ card.js
@@ -386,6 +386,20 @@ function draw() {
 	} else {
 		cursor('grab');
 	}
+
+	// Pinch-to-zoom detection (for touch)
+	if (touches.length == 2) {
+		let currDist = dist(touches[0].x, touches[0].y, touches[1].x, touches[1].y);
+		
+		if (prevDist > 0) {
+			let zoomAmount = (prevDist - currDist) * 0.05; // Sensitivity for zoom
+			cam.move(0, 0, zoomAmount);  // Adjust camera zoom along Z-axis
+		}
+		
+		prevDist = currDist;  // Update previous distance for next frame
+	} else {
+		prevDist = -1;  // Reset when not pinching
+	}
 }
 
 function mousePressed() {
